61c87dc2c7084a8d849f8a1b110e53c39884aab8
Ali Yasin Eser <aliyasineser@gmail.com>
2021-03-23 16:33:37 +0300

    .gitignore is now working

--- Pods/Target Support Files/Pods-Watchlist/Pods-Watchlist-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Watchlist : NSObject
-@end
-@implementation PodsDummy_Pods_Watchlist
-@end
--- Pods/Target Support Files/TMDBSwift/TMDBSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TMDBSwift : NSObject
-@end
-@implementation PodsDummy_TMDBSwift
-@end
